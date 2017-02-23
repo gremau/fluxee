dc5e88e806ccb75adc84569f2e758af36d1b374f
Greg Maurer <gregmaurer@gmail.com>
2017-02-22 20:11:47 -0700

    new functions and improvements

--- datatools.py
+++ datatools.py
@@ -1,4 +1,8 @@
 import pdb
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
 def measurement_h_v_dict(cols, meas, str_exclude=None):
     '''
     Extract measurement horizontal and vertical location configuration. This
@@ -28,3 +32,36 @@ def measurement_h_v_dict(cols, meas, str_exclude=None):
         hv_dict[meas + '_' + pnum].append(vert[i])
 
     return hv_dict
+
+def gapfill_series(s_withgaps, s_gapfiller, makeplots=False):
+    '''
+    Fill nans in one series with data from another
+
+    IN:
+        s_withgaps  : (pandas series) series containing gaps
+        s_gapfiller : (pandas series) series (continuous) used to fill gaps 
+    
+    OUT:
+        s_filled    : pandas series) series with gaps filled
+    '''
+
+    s_filled = pd.DataFrame(s_withgaps.copy())
+
+    if s_withgaps.index.equals(s_gapfiller.index):
+        gapfill = np.isnan(s_withgaps)
+        s_filled.columns = [s_filled.columns[0] + '_gf']
+        s_filled.loc[gapfill, s_filled.columns] = s_gapfiller[gapfill]
+        s_filled[s_filled.columns[0] + 'FLAG'] = gapfill
+    else:
+        print('Error - indices are not the same')
+    if makeplots:
+        f, ax = plt.subplots(1)
+        ax.set_title(s_withgaps.name)
+        ax.plot(s_filled.iloc[:,0], 'og', mfc='w')
+        ax.plot(s_gapfiller, '.r', ms=3)
+        ax.plot(s_withgaps, '.b', ms=3)
+        ax.set_ylabel('Y')
+        ax.legend(['filled series', 'gapfill data', 'original series'], ncol=3)
+
+    return s_filled
+    
