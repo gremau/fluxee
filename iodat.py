211502401b3b6b49b4668fef8c55d69eec9732fc
Greg Maurer <gregmaurer@gmail.com>
2017-04-27 15:13:49 -0600

    raw variable renamer works

--- iodat.py
+++ iodat.py
@@ -12,7 +12,7 @@ import yaml
 import os
 import pdb
 
-def get_file_collection(sitename, datpath):
+def get_file_collection(sitename, datpath, ext='.dat'):
     """
     Read a list of files from a data directory, match against desired site,
     and return the list and the collection dates of each file. This function
@@ -21,12 +21,13 @@ def get_file_collection(sitename, datpath):
 
     MNPclimoseq_Creosote_2017_03_17_11_55_00.dat
 
-    Only returns file matching the sitename string.
+    Only returns files matching the sitename and extension (.dat by 
+    default) strings.
     """
     # Get a list of filenames in provided data directory
     files = os.listdir(datpath)
     # Select desired files from the list (by site)
-    site_files = [ f for f in files if sitename in f ]
+    site_files = [ f for f in files if sitename in f and ext in f ]
     # Get collection date for each file. The collection date is in the 
     # filename with fields delimited by '_'
     collect_dt = []
@@ -37,11 +38,29 @@ def get_file_collection(sitename, datpath):
     return site_files, collect_dt
 
 
-def read_yaml_conf(site, yamltype, confdir='./ecoflux_config'):
-    yamlfile = os.path.join(confdir, site, yamltype + '.yaml')
+def read_yaml_conf(sitename, yamltype, confdir='ecoflux_config'):
+    """
+    Read a specified YAML configuration file from a given site's ecoflux
+    configuration directory. Checks the YAML file meta dictionary to ensure
+    configuration is for the correct site and type.
+
+    Args:
+        sitename (string): site identifier
+        yamltype (string): identifies the YAML filename and configuration type
+        confdir (string): directory to look for YAML configuration files
+
+    Returns:
+        yamlf (dict): Returns the "items" dictionary of configuration values
+                      from the YAML file
+
+    Raises:
+        ValueError: Raises ValueError if the sitename or yamltype parameters
+                    do not match those found in the yaml file
+    """
+    yamlfile = os.path.join(confdir, sitename, yamltype + '.yaml')
     stream = open(yamlfile, 'r')
     yamlf = yaml.load(stream)
-    ysite = yamlf['meta']['site']==site
+    ysite = yamlf['meta']['site']==sitename
     ytype = yamlf['meta']['conftype']==yamltype
     if not(ysite) or not(ytype):
         raise ValueError('YAML file site/type mismatch.')
@@ -49,6 +68,8 @@ def read_yaml_conf(site, yamltype, confdir='./ecoflux_config'):
         return yamlf['items']
 
 def calculate_freq(idx):
+    """
+    """
     cfreq = (idx.max()-idx.min())/(len(idx)-1)
     cfreq = cfreq.seconds/60
     print("Calculated frequency is " + "{:5.3f}".format(cfreq) + " minutes")
@@ -148,11 +169,11 @@ def site_datafile_concat(sitename, datpath, iofunc=load_toa5):
     """
             
     # Get list of files and thier collection dates from the provided directory
-    site_files, collect_dt = get_file_collection(sitename, datpath)
+    files, collect_dt = get_file_collection(sitename, datpath)
     # Initialize DataFrame
     sitedf = pd.DataFrame()
     # Loop through each year and fill the dataframe
-    for i in site_files:
+    for i in files:
         # Call load_toa5_file
         filedf = iofunc(datapath + i)
         # And append to site_df, 'verify_integrity' warns if there are
@@ -175,27 +196,46 @@ def site_datafile_concat(sitename, datpath, iofunc=load_toa5):
     sitedf = sitedf.reindex( fullidx )
     return sitedf, collect_dt
 
-def rename_raw_variables(sitename, rawpath, rnpath, confdir='./ecoflux_config'):
+
+def rename_raw_variables(sitename, rawpath, rnpath, confdir='ecoflux_config'):
     """
+    Rename raw datalogger variables according to YAML configuration file
+
+    Args:
+        sitename (string): site identifier
+        rawpath (string): raw datalogger file path
+        rnpath (string): path to write renamed data files
+        confdir (string): directory to look for YAML configuration files
+
+    Returns:
+        Does not return anything but writes new files in rnpath
     """
     # Get var_rename configuration file for site
     yamlf = read_yaml_conf(sitename, 'var_rename', confdir=confdir)
     # Get list of files and their collection dates from the raw directory
-    site_files, collect_dt = get_file_collection(sitename, rawpath)
+    files, collected = get_file_collection(sitename, rawpath)
+    # Loop through each rename event and change the file
+    for i, filename in enumerate(files):
+        findvars, newvars = ([],[])
+        # For each rename event, add variable changes to findvar/repvar
+        # if the changes occurred after the file collection date
+        for j, key in enumerate(sorted(yamlf.keys())):
+            rn = yamlf[key] # Get rename event j
+            if rn["first_changed_dt"] > collected[i]:
+                findvars = findvars + rn["from"]
+                newvars = newvars + rn["to"]
 
-    collect_dt = np.array(collect_dt)
-    for i in sorted(yamlf.keys()):
-        rn = yamlf[i]
-        rn_dts = collect_dt<rn['first_changed_dt']
-        rn_files = [site_files[x] for x in range(len(site_files)) if rn_dts[x]]
-    # Read in the file
-    with open('file.txt', 'r') as file :
-        filedata = file.read()
-    # Replace the target string
-    filedata = filedata.replace('ram', 'abcd')
-    # Write the file out again
-    with open('file.txt', 'w') as file:
-        file.write(filedata)
+        # Now read in file, replace target strings, write a new file
+        filepath = os.path.join(rawpath, filename)
+        with open(filepath, 'r') as file:
+            filedata = file.read()
+        for k, findvar in enumerate(findvars):
+            newvar = newvars[k]
+            filedata = filedata.replace(findvar, newvar)
+        rn_filepath = os.path.join(rnpath, filename)
+        with open(rn_filepath, 'w') as file:
+            file.write(filedata)
+
 
 def ecoflux_out(df,foutpath, sitename='Undefined', cscript='Undefined'):
     """
