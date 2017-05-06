a9e641c85987932e1bb565df22897edec8a442d2
Greg Maurer <gregmaurer@gmail.com>
2017-05-05 23:36:33 -0600

    Improvements to data flagging system
    * QA functions moved to new module
    * Functions and arguments are specified in qa_flag yaml file
    * Allows creation of data qa transformations, not just removal
    * Returned flags refer to items in the qa_flag yaml

---
+++ qafunctions.py
@@ -0,0 +1,36 @@
+"""
+Functions that can be called to qa a dataframe. These are generally called from
+the apply_qa_flags function in the flag module. Functions must return a 
+dataframe (often the same as the input), a boolean array mask indicating which
+dataframe values the qa flag points to, and a boolean value indicating whether
+the flagged data should be masked (True = flag for removal).
+"""
+
+import pandas as pd
+import numpy as np
+import pdb
+
+def dtrange_rm_all(df, idxrange, colrange):
+    """
+    Mask all matching idxrange and colrange
+    """
+    mask = pd.DataFrame(False, index=df.index, columns=df.columns)
+    mask.loc[idxrange, colrange] = True
+    return [df, mask, True]
+
+def dtrange_rm_thresholdvar(df, idxrange, colrange, threshvar,
+        direction, threshval):
+    """
+    Mask values in matching idxrange and colrange AND where another variable
+    (threshvar) is above/below threshval 
+    """
+    mask = pd.DataFrame(False, index=df.index, columns=df.columns)
+    if direction=='above':
+        idxrange_thv = np.logical_and(idxrange, df[threshvar] > threshval)
+    elif direction=='below':
+        idxrange_thv = np.logical_and(idxrange, df[threshvar] < threshval)
+    else:
+        raise ValueError('Incorrect direction')
+    mask.loc[idxrange_thv, colrange] = True
+    return [df, mask, True]
+
